28c56db476f230ccea8fab7efffe90a2acaf3aea
Andrey <GusevAndrey1988@gmail.com>
2023-09-16 14:56:42 +0300

    upd

--- API/Controllers/ForumController.cs
+++ API/Controllers/ForumController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Storage;
 
 namespace API.Controllers
 {
@@ -10,6 +12,21 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ForumController : ControllerBase
     {
-        
+        /// <summary>
+        /// Get list of every forum
+        /// </summary>
+        /// <param name="dbContext"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(string[]))]
+        public async Task<IActionResult> GetForums(
+            [FromServices] ForumDbContext dbContext,
+            CancellationToken cancellationToken
+        )
+        {
+            var forumTitles = await dbContext.Forums.Select(f => f.Title).ToArrayAsync(cancellationToken);
+            return Ok(forumTitles);
+        }
     }
 }
